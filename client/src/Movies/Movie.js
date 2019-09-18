369a30dcca76a6e10851f17fa4ddd80e50f3f82b
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-09-17 18:14:14 -0700

    MVP completed

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -4,13 +4,13 @@ import axios from 'axios';
 const Movie = (props) => {
   const [movie, setMovie] = useState();
  
-  const id = props.match.params.id;
+  
 
   useEffect(() => {
     
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
-
+    const id = props.match.params.id;
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
@@ -20,7 +20,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[id]);
+  },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
